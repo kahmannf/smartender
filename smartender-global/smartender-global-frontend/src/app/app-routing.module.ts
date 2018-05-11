3360f958a621da41dba1ab43a2c2ebaa24ba6d78
kahmannf <felix@kahmann.de>
2018-05-11 04:35:52 +0200

    fix routing

--- smartender-global/smartender-global-frontend/src/app/app-routing.module.ts
+++ smartender-global/smartender-global-frontend/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { DashboardComponent } from './_modules/dashboard/dashboard.component';
 import { CompleteRegisterComponent } from './_modules/complete-register/complete-register.component';
 import { AuthGuard } from './guards/auth.guard';
 import { HomeComponent } from './_modules/home/home.component';
@@ -7,11 +8,16 @@ import { LoginComponent } from './_modules/login/login.component';
 import { RegisterComponent } from './_modules/register/register.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
+  { path: '', pathMatch: 'full', redirectTo: '/home'},
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard],
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+      { path: 'dashboard', component: DashboardComponent},
+      { path: '**', redirectTo: 'dashboard' }
+    ]},
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'complete-register/:registerkey', component: CompleteRegisterComponent },
-  { path: '', pathMatch: 'full', redirectTo: '/home'},
   { path: '**', redirectTo: '/home'},
 ];
 
