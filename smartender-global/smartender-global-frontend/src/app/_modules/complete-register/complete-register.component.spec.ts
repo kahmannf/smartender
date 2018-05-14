361a0d6f519377d2a0186e2d01cebb1a2c47175d
kahmannf <felix@kahmann.de>
2018-05-14 16:14:40 +0200

    add unit tests for complet-register component

--- smartender-global/smartender-global-frontend/src/app/_modules/complete-register/complete-register.component.spec.ts
+++ smartender-global/smartender-global-frontend/src/app/_modules/complete-register/complete-register.component.spec.ts
@@ -1,14 +1,29 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { AuthService } from './../../shared/auth.service';
+import { HttpClientModule } from '@angular/common/http';
+import { UserService } from './../../shared/user.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { async, ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
 
 import { CompleteRegisterComponent } from './complete-register.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('CompleteRegisterComponent', () => {
   let component: CompleteRegisterComponent;
   let fixture: ComponentFixture<CompleteRegisterComponent>;
+  let debugElement: DebugElement;
+  let authService: AuthService;
+  let userService: UserService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CompleteRegisterComponent ]
+      declarations: [ CompleteRegisterComponent ],
+      imports: [
+        ReactiveFormsModule,
+        RouterTestingModule,
+        HttpClientModule
+      ]
     })
     .compileComponents();
   }));
@@ -16,10 +31,57 @@ describe('CompleteRegisterComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CompleteRegisterComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
     fixture.detectChanges();
+
+    authService = debugElement.injector.get(AuthService);
+    userService = debugElement.injector.get(UserService);
+
+    spyOn(authService, 'activateAccount');
+    spyOn(component, 'passwordValidator');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should be invalid by default', () => {
+    expect(component.registerForm.invalid).toBeTruthy();
+  });
+
+  it('should expect a password to be 6 characters long', () => {
+    const password = component.registerForm.controls['password'];
+    password.setValue('12345');
+
+    expect(password.errors['minlength']).toBeTruthy();
+
+    password.setValue('123456');
+
+    expect(password.valid).toBeTruthy();
+  });
+
+  it('should deny not matching passwords', () => {
+    const password = component.registerForm.controls['password'];
+    const passwordRepeat = component.registerForm.controls['passwordRepeat'];
+
+    password.setValue('123456');
+    passwordRepeat.setValue('123457');
+
+
+    expect(passwordRepeat.invalid).toBeTruthy();
+  });
+
+  it('should accept matching passwords', fakeAsync(() => {
+    const password = component.registerForm.controls['password'];
+    const passwordRepeat = component.registerForm.controls['passwordRepeat'];
+
+    password.setValue('123456');
+    passwordRepeat.setValue('123456');
+
+
+    expect(passwordRepeat.valid).toBeTruthy();
+  }));
+
 });
+
+
