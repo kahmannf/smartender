b4ed927cecf53c3d085194b3c1dc708a83640613
kahmannf <felix@kahmann.de>
2018-05-17 19:58:07 +0200

    move public/machine to machine_service

--- smartender-global/smartender-global-backend/routes/machine_service/index.js
+++ smartender-global/smartender-global-backend/routes/machine_service/index.js
@@ -11,25 +11,21 @@ const init = (socket_io) => {
   io = socket_io;
 };
 
-router.get('/init/:key', (req, res) => {
-  if(req.params.key) {
-    machine_lib.getMachineByKey(req.params.key)
+router.get('/report/:machinekey', (req, res) => {
+  if(req.params.machinekey) {
+    machine_lib.reportMachine(req.params.machinekey)
     .then(result => {
-      if(result) {
-        result.machinekey = undefined;
-
-        io.emit('machine ' + result.id, result);
-      }
-      res.end();
+      res.json(result);
     })
     .catch(err => {
       logger.error(err, 500);
       res.sendStatus(500);
-    });
+    })
   } else {
     res.sendStatus(400);
   }
 });
+
 
 module.exports = { 
   router,
