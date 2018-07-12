d0e3e22b29c10edd479278859121589e1cc6666f
Felix Kahmann <felix@kahmann.de>
2018-07-12 22:36:45 +0200

    initialize arduino test project and empty sketch

---
+++ smartender-machine/playground/SerialTestApp/SerialTestApp/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 12.07.2018 16:57:49
+ * Author : Laptop
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
---
+++ smartender-machine/playground/SerialTestApp/SerialTestApp/empty_sketch/Sketch.cpp
@@ -0,0 +1,44 @@
+﻿/*Begining of Auto generated code by Atmel studio */
+#include <Arduino.h>
+
+/*End of auto generated code by Atmel studio */
+
+
+//Beginning of Auto generated function prototypes by Atmel Studio
+//End of Auto generated function prototypes by Atmel Studio
+
+int slot = 3;
+
+void setup() {
+  Serial.begin(9600, SERIAL_8N1);
+
+	DDRD = 0b00111100;
+	//DDRB = 0xFF;
+
+	PORTD = 0b11111100;
+
+	//PORTB = 0b00111111;
+	Serial.write(0x30);
+	Serial.write(0x30);
+	Serial.write(0x30);
+	Serial.write(0x30);
+	Serial.flush();
+}
+
+void loop() {
+  
+  
+	if(Serial.available())
+	{
+		int value = Serial.read();
+		
+		if(value == 0x30)
+		{
+			PORTD |= (1<<(slot));
+		}
+		else
+		{
+			PORTD &= ~(1<<(slot));
+		}
+	}
+}
